5e9bc5c87e8f03503159db9b576a0ceba0fe2e0d
cristina <csuarezf09@gmail.com>
2024-02-22 13:55:05 -0600

    Update Footer

--- healthsync/components/Footer.tsx
+++ healthsync/components/Footer.tsx
@@ -5,14 +5,10 @@ const Footer = () => (
   <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-100'>
     <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
       <p>@2024 HealthSync. All rights reserved</p>
+      <Link href="/" className="text-gray-500"> Privacy & Policy Terms & Condition </Link>
 
       <div className="footer__copyrights-link">
-        <Link href="/" className="text-gray-500">
-          Privacy & Policy
-        </Link>
-        <Link href="/" className="text-gray-500">
-          Terms & Condition
-        </Link>
+      <p>Adriana Medina Nuñez <br /> Isabel Beatriz Franco Díaz <br /> Cristina Suárez Franco </p>
       </div>
     </div>
   </footer>
